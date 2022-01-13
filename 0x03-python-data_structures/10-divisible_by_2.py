cbf92bfa333660859a8c67c4947838c3f2f6dab8
zedu3 <zelalem138@gmail.com>
2022-01-13 06:16:58 -0800

    divisible ?

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+
+    new_list = []
+
+    if len(my_list) is 0:
+        return new_list
+
+    for i in my_list:
+        if i % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+
+    return new_list
