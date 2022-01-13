0aa598438c7b1522199b55d1bc950f4e3a1e846d
zedu3 <zelalem138@gmail.com>
2022-01-13 05:24:21 -0800

    removed c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new = list(my_string)
+    new_list = ''
+    for char in new:
+        if char != 'c' and char != 'C':
+            new_list+=char
+    return new_list
+if __name__ == '__main__':
+    print(no_c("Best School"))
+    print(no_c("Chicago"))
+    print(no_c("C is fun!"))
