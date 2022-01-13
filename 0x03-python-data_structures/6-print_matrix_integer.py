496f48522818b3450a2e0c2f16d859f5eb043675
zedu3 <zelalem138@gmail.com>
2022-01-13 05:39:31 -0800

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,13 +7,3 @@ def print_matrix_integer(matrix=[[]]):
             if j != len(matrix[i]) - 1:
                 print(" ", end="")
         print()
-if __name__ == '__main__':
-    matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-    ]
-
-    print_matrix_integer(matrix)
-    print("--")
-    print_matrix_integer()
