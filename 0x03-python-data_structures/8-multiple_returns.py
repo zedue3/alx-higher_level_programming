f3c8ace260431e71d591f8a2abf290b6e07255b8
zedu3 <zelalem138@gmail.com>
2022-01-13 05:57:31 -0800

    done

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+
+
 def multiple_returns(sentence):
+    if sentence is not None:
+        length = len(sentence)
+        if length == 0:
+            return (length, None)
+        return (length, sentence[0])
 
-    if len(sentence) is 0:
-        return (0, None)
 
-    return (len(sentence), sentence[0])
+if __name__ == '__main__':
+    sentence = "At Holberton school, I learnt C!"
+    length, first = multiple_returns(sentence)
+    print("Length: {:d} - First character: {}".format(length, first))
