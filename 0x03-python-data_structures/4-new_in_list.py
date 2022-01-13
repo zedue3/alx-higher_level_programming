c99af891d7994a6df388835e3b1d902504063c54
zedu3 <zelalem138@gmail.com>
2022-01-13 05:09:12 -0800

    copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+
+    new_list = my_list.copy()
+
+    if idx >= len(my_list) or idx < 0:
+        return new_list
+
+    new_list[idx] = element
+    return new_list
