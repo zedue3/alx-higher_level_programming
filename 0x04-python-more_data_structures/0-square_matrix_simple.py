cfa08e15ccbe07a6b772cdc640ef7c9e58853514
zedu3 <zelalem138@gmail.com>
2022-01-22 01:06:55 -0800

    done squaring a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
     new_matrix = list(map(lambda i: list(map(lambda j: j ** 2, i)), matrix))
-    return new_matrix
+    return(new_matrix)
