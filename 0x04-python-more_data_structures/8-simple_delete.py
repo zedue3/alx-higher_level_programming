771dfb2717cf289d98dbd3eba25b5b9a7ebba6a3
zedu3 <zelalem138@gmail.com>
2022-01-23 08:36:38 -0800

    deleted with a key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    new_dictionary = del a_dictionary[key]
+    if key in a_dictionary:
+        new_dictionary = del a_dictionary[key]
     return (new_dictionary)
