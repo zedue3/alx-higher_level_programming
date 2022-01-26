9d24b168d853ff4de135fd7717b121b2ecb09d32
zedu3 <zelalem138@gmail.com>
2022-01-26 05:46:36 -0800

    safe print divission

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+        return result
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+if __name__ == "__main__":
+    safe_print_division = __import__('3-safe_print_division').safe_print_division
+
+    a = 12
+    b = 2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
+
+    a = 12
+    b = 0
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
