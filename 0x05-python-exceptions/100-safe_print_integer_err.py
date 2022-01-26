6b1024159e5ed482d1413d65aebf631b5e721d48
zedu3 <zelalem138@gmail.com>
2022-01-26 11:51:08 -0800

    exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
     try:
-        return value
-    except:
-        print("Exception: Unknown format code \'d\' for object of type \'str\'")
+        print('{:d}'.format(value))
+        return True
+    except Exception as err:
+        err = "Exception: " + str(err) + "\n"
+        sys.stderr.write(err)
         return False
-if __name__ == "__main__":
-    safe_print_integer_err = \
-    __import__('100-safe_print_integer_err').safe_print_integer_err
 
+
+if __name__ == '__main__':
     value = 89
     has_been_print = safe_print_integer_err(value)
     if not has_been_print:
@@ -19,7 +23,7 @@ if __name__ == "__main__":
     if not has_been_print:
         print("{} is not an integer".format(value))
 
-    value = "School"
+    value = "Holberton"
     has_been_print = safe_print_integer_err(value)
     if not has_been_print:
         print("{} is not an integer".format(value))
