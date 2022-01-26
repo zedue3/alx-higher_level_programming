2341f5cfcccedcadebdef6e6fc24f5d69f8a797c
zedu3 <zelalem138@gmail.com>
2022-01-26 11:39:21 -0800

    raised an exception

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
     raise NameError(message)
-if __name__ == "__main__":
-    raise_exception_msg = __import__('6-raise_exception_msg').raise_exception_msg
-
-    try:
-        raise_exception_msg("C is fun")
-    except NameError as ne:
-        print(ne)
