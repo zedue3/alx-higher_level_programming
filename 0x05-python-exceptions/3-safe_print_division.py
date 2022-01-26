b3ad8a6efc852e47a36ae98afc7d5f415823b1a7
zedu3 <zelalem138@gmail.com>
2022-01-26 06:18:19 -0800

    exception for divission

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,14 +2,12 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        return(result)
     except ZeroDivisionError:
         result = None
     finally:
-        print("Inside result: {}".format(result))
+        print('Inside result: {}'.format(result))
+    return result
 if __name__ == "__main__":
-    safe_print_division = __import__('3-safe_print_division').safe_print_division
-
     a = 12
     b = 2
     result = safe_print_division(a, b)
