7e97bfb764560cc421e821590ca8307ee2d4775a
zedu3 <zelalem138@gmail.com>
2022-01-25 23:18:29 -0800

    exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if isinstance(value,int):
-        try:
-            print("{:d}".format(value))
-            return True
-        except:
-            return False
+    try:
+        print("{:d}".format(value))
+        return True
+    except:
+        return False
 if __name__ == "__main__":
     safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
 
